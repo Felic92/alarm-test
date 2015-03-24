4cd023424f8830bfa7a227e2b258eb28e2434388
Michael Feliciano <mfeliciano.enrique@gmail.com>
2015-03-23 22:21:58 -0400

    Added basic ability to select times and have them appended to the screen

--- www/js/app.js
+++ www/js/app.js
@@ -13,6 +13,36 @@ angular.module('wakeup', ['ionic'])
   });
 })
 
+.controller('createAlarmCtrl', function($scope,$ionicPopup){
+
+    $scope.alarms = [];
+
+    $scope.createAlarm = function(alarmTime){
+      $scope.alarms.push(alarmTime);
+    };
+    
+    $scope.newAlarm = function(){
+
+      $scope.data = {};
+
+      $ionicPopup.show({
+        template: '<input type="time" ng-model="data.alarmTime"> ',
+        title: 'Set Time',
+        scope: $scope,       
+        buttons: 
+        [{text: '<b>Done</b>', type: 'button-royal',
+          onTap: function(e){  //dont allow user to close unless an alarm is created if created scroll top
+            !$scope.data.alarmTime ? e.preventDefault() : $scope.createAlarm($scope.data.alarmTime);
+          }
+        }]
+      });
+
+    };
+
+
+
+
+})
 
 
 

e322df1d6ab27e69478237cb0d33b0b5d1f2fab0
Michael Feliciano <mfeliciano.enrique@gmail.com>
2015-03-24 19:46:10 -0400

    Added AlarmsFactory for easy access to alarm related methods and alarm creation

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,33 @@
 angular.module('wakeup', ['ionic'])
 
+.factory('Alarms', function AlarmFactory(){
+  return {
+    all : function() {
+      var alarms = window.localStorage['Alarms'];
+      if(alarms){
+        return angular.fromJson(alarms)
+      }
+      return [];
+    },
+    save: function(alarms){
+      window.localStorage['Alarms'] = angular.toJson(alarms);
+    },
+    createAlarm : function(alarmTime,alarmId){
+      return {
+        id : alarmId,
+        label : 'Label',
+        time : alarmTime
+      };
+    },
+    getNextAlarmId : function(){
+      return parseInt(window.localStorage['nextAlarmId']) || 0;
+    },
+    setNextAlarmId : function(id){
+      window.localStorage['nextAlarmId'] = id;
+    }
+  }
+})
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -13,34 +41,43 @@ angular.module('wakeup', ['ionic'])
   });
 })
 
-.controller('createAlarmCtrl', function($scope, $ionicPopup){
+.controller('createAlarmCtrl', function($scope, $ionicPopup, Alarms){
 
-    //Give each alarm a unique id
-    var alarmId = 0;
-
-    //List to store alarm objects
-    $scope.alarms = [];
+    //Load or create list to store alarm objects
+    $scope.alarms = Alarms.all();
 
     //Allows user to create and append new alarm objects to the existing list 
     $scope.newAlarm = function(){
 
-      $scope.alarm = {id: alarmId, label: 'Label'};
+      $scope.data = {};
 
       $ionicPopup.show({
-        template: '<input type="time" ng-model="alarm.time"> ',
+        template: '<input type="time" ng-model="data.time"> ',
         title: 'Set Time',
         scope: $scope,       
         buttons: 
         [{text: 'Cancel'}, {text: '<b>Done</b>', type: 'button-positive',
-          onTap: function(e){  //dont allow user to create unless an alarm time is set
-            !$scope.alarm.time ? e.preventDefault() : $scope.alarms.push($scope.alarm);
+          onTap: function(e){  
+            if(!$scope.data.time){ //dont allow user to create unless an alarm time is set
+              e.preventDefault();
+            }else{
+              var id = Alarms.getNextAlarmId();
+              var time = $scope.data.time; 
+
+              $scope.alarms.push(Alarms.createAlarm(time,id));
+              Alarms.setNextAlarmId(id++);
+              Alarms.save($scope.alarms);
+            }
           }
         }]
       });
 
-      alarmId++; 
-
     };
+})
+
+.controller('manageAlarmCtrl', function($scope){
+
+
 
 })
 
